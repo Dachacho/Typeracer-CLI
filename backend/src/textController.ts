e072f8e704b81cbe19a5585c075ef8420c93b074
Dachacho <arevadzedachi123@gmail.com>
2025-09-05 16:03:33 +0400

    added compare function

--- backend/src/textController.ts
+++ backend/src/textController.ts
@@ -15,6 +15,25 @@ export const getText = async (req: Request, res: Response) => {
   }
 };
 
-export const finishText = async (req: Request, res: Response) => {};
+export const finishText = async (req: Request, res: Response) => {
+  const { username, textId, timeTaken, userInput } = req.body;
+
+  const originalText = await prisma.text.findFirst(textId);
+};
 
 export const getLeaderboard = async (req: Request, res: Response) => {};
+
+function compare(original: string, input: string) {
+  const originalArr = original.split("");
+  const inputArr = input.split("");
+
+  let correct = 0;
+  for (let i = 0; i < originalArr.length; i++) {
+    if (inputArr[i] === originalArr[i]) {
+      correct++;
+    }
+  }
+
+  const accuracy = correct / originalArr.length;
+  return accuracy;
+}
