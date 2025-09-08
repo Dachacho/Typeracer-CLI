afec0e7425ce2f4f7c893ada816a8275792343c0
Dachacho <arevadzedachi123@gmail.com>
2025-09-09 01:23:41 +0400

    add createRoom endpoint

--- backend/src/controllers/roomController.ts
+++ backend/src/controllers/roomController.ts
@@ -3,9 +3,26 @@ import type { Request, Response } from "express";
 import compare from "../utils/util.ts";
 
 export const createRoom = async (req: Request, res: Response) => {
-  const count = await prisma.text.count();
-  const randomIndex = Math.floor(Math.random() * count);
-  const randomText = await prisma.text.findFirst({
-    skip: randomIndex,
-  });
+  try {
+    const count = await prisma.text.count();
+    const randomIndex = Math.floor(Math.random() * count);
+    const randomText = await prisma.text.findFirst({
+      skip: randomIndex,
+    });
+
+    if (!randomText) {
+      return res.status(500).json({ message: "failed to get text" });
+    }
+
+    const room = await prisma.room.create({
+      data: {
+        textId: randomText.id,
+        status: "waiting",
+      },
+    });
+
+    res.json(room);
+  } catch (err: any) {
+    res.json(500).json({ message: err.message });
+  }
 };
