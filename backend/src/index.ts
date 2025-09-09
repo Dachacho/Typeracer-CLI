cd767b0d532d0239130013a4e162003a315d22b6
Dachacho <arevadzedachi123@gmail.com>
2025-09-09 23:33:18 +0400

    add socket.io events

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,51 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import textRouter from "./routes/textRouter.ts";
 import roomRouter from "./routes/roomRouter.ts";
 
 dotenv.config();
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: { origin: "*" },
+});
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
 
+io.on("connection", (socket) => {
+  console.log("user conneted: ", socket.id);
+
+  socket.on("joinRoom", (roomId) => {
+    socket.join(`room-${roomId}`);
+    io.to(`room-${roomId}`).emit("userJoined", socket.id);
+  });
+
+  socket.on("startRace", (roomId) => {
+    io.to(`room-${roomId}`).emit("raceStarted");
+  });
+
+  socket.on("finishRace", (roomId, username, wpm, accuracy) => {
+    io.to(`room-${roomId}`).emit("userFinished", { username, wpm, accuracy });
+  });
+});
+
 app.use(textRouter);
 app.use(roomRouter);
 
-app.get("/", (req, res) => {
-  res.send("backend is running");
+// app.get("/", (req, res) => {
+//   res.send("backend is running");
+// });
+
+httpServer.listen(port, () => {
+  console.log(`server running on ${port}`);
 });
 
-app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`server is running on port ${port}`);
+// });
